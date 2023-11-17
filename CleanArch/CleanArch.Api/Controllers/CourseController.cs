0eafe5b0f292f5b12ecedf499b4450a86751e65d
Gabriel Herrera <gabriel.herrera@jbknowledge.com>
2023-11-17 07:32:01 -0300

    Commit Before AutoMapperDemo

--- CleanArch/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -1,3 +1,5 @@
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Api.Controllers
@@ -6,6 +8,19 @@ namespace CleanArch.Api.Controllers
 	[Route("[controller]")]
 	public class CourseController : ControllerBase
 	{
+		private readonly ICourseService _courseService;
+
+		public CourseController(ICourseService courseService) 
+		{ 
+			_courseService = courseService;	
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+		{
+			_courseService.Create(courseViewModel);
+			return Ok();
+		}
 
 	}
 }
